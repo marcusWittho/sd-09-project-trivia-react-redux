535dc21d7a58b9227c1c697f37569535c7de1844
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-04-06 16:41:20 -0400

    requisito 1 pronto

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -7,23 +7,53 @@ class Login extends React.Component {
     this.state = {
       player: '',
       email: '',
-    }
-    
+      playButton: false,
+    };
+
     this.onChange = this.onChange.bind(this);
   }
 
   onChange({ target }) {
-    this.setState({  })
+    const { id, value } = target;
+    this.setState({ [id]: value }, () => this.validateFields());
+  }
+
+  validateFields() {
+    const { player, email } = this.state;
+    const regex = /^[\w.]+@[a-z]+\.\w{2,3}$/g;
+    const playButton = player.length > 0 && regex.test(email);
+    this.setState({ playButton });
   }
 
   render() {
+    const { playButton } = this.state;
     return (
       <div>
-        <label htmlFor="player">Name</label>
-        <input type="text" id="player" data-testid="input-player-name" onChange={() => this.onChange('player')} />
-        <label htmlFor="email">Email</label>
-        <input type="text" id="email" data-testid="input-gravatar-email" onChange={this.onChange} />
-        <button type="button" data-testid="btn-play">Play!</button>
+        <label htmlFor="player">
+          Name
+          <input
+            type="text"
+            id="player"
+            data-testid="input-player-name"
+            onChange={ this.onChange }
+          />
+        </label>
+        <label htmlFor="email">
+          Email
+          <input
+            type="text"
+            id="email"
+            data-testid="input-gravatar-email"
+            onChange={ this.onChange }
+          />
+        </label>
+        <button
+          type="button"
+          data-testid="btn-play"
+          disabled={ !playButton }
+        >
+          Play!
+        </button>
       </div>
     );
   }
