f18a60bb348512b59bedf6c88f9fd6d52892cfb2
Demistocles <demi.treze@gmail.com>
2021-04-06 17:39:39 -0300

    adiciona requisito 3

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
-// import { Switch, Route } from 'react-router-dom';
-import logo from './trivia.png';
-import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import Config from './pages/Config';
+import Home from './pages/Home';
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          SUA VEZ
-        </p>
-      </header>
-    </div>
-  );
+    <Switch>
+      <Route exact path="/" component={ Home } />
+      <Route path="/settings" component={ Config } />
+    </Switch>
+  )
 }
