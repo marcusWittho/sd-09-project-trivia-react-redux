e60ee99e276075c23b47f69806b18d075cc26d38
fernandacajueiro <fernandacajueiromachado@gmail.com>
2021-04-07 00:21:17 +0200

    Adds feature 3

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import Play from './pages/Play';
+import Settings from './pages/Settings';
 
 class App extends React.Component {
   render() {
@@ -10,6 +11,7 @@ class App extends React.Component {
       <Switch>
         <Route exact path="/" component={ Home } />
         <Route exact path="/play" component={ Play } />
+        <Route exact path="/settings" component={ Settings } />
       </Switch>
     );
   }
