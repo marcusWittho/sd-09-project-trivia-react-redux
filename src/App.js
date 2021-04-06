10e04f2fc7e0ad6431e2b176913113fcc7b7cead
Marcelo Campos <marcelo.campos.economista@gmail.com>
2021-04-06 17:34:40 -0300

    Requisito 1 completo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        OLA!
-      </div>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+      </Switch>
     );
   }
 }
