54939e87f90d807d0a73c0cfb0c48bfa7d199277
Leticia Lima <letylima96@gmail.com>
2021-04-06 19:53:55 -0300

    Configura Redux

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,6 @@ import { createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from '../reducers';
 
-const store = createStore(rootReducer, composeWithDevTools);
+const store = createStore(rootReducer, composeWithDevTools());
 
 export default store;
