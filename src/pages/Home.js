b3089d4f13cf5837ca42192e96dcd71599fdbc03
richardfazeredo <richardcompletetour@gmail.com>
2021-04-06 17:34:30 -0300

    Requisito 1

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Login from '../components/login';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <Login />
+    );
+  }
+}
+
+export default Home;
