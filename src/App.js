7127e2e9a3cf85c31d59e0d199cccc9af7a21070
Thiago Carreira Vallim <thiagocvallim@gmail.com>
2021-04-06 16:23:44 -0400

    Adiciona tela de jogo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
-import Login from './Pages/Login';
 import './App.css';
+import Login from './Pages/Login';
+import GameScreen from './Pages/GameScreen';
 
 class App extends React.Component {
   render() {
     return (
       <Switch>
         <Route exact path="/" component={ Login } />
+        <Route path="/trivia" component={ GameScreen } />
       </Switch>
     );
   }
---
+++ src/Pages/GameScreen.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class GameScreen extends Component {
+  render() {
+    return (
+      <div>
+        GameScreen
+      </div>
+    );
+  }
+}
+
+export default GameScreen;
