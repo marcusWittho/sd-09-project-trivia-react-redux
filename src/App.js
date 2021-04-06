f333d57518ec3240372edc8212a8116b4b7f4836
Diogenes <diogenesfelipe.s@outlook.com>
2021-04-06 17:25:11 -0300

    feat/Requirement 3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login/Login';
+import Settings from './pages/Settings/Settings';
 import './App.css';
 
 export default function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
+      <Route path="/settings" component={ Settings } />
     </Switch>
   );
 }
