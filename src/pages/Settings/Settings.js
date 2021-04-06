351d74e845087281dca511191c83fb09426844da
Diogenes <diogenesfelipe.s@outlook.com>
2021-04-06 17:32:42 -0300

    feat/css-initial-settings

--- src/pages/Settings/Settings.js
+++ src/pages/Settings/Settings.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Settings extends React.Component {
   render() {
     return (
-      <div>
+      <div className="settings-container">
         <h1 data-testid="settings-title">Settings</h1>
       </div>
     );
