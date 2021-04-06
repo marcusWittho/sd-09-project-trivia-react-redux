1b16a688817ff054ddbca84d480cc4eba3f0bc77
Nícolas Tozato <ntozato9@gmail.com>
2021-04-06 19:52:58 -0300

    adiciona link no botão

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Settings from './pages/Settings';
+import Game from './pages/Game';
 
 export default function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
       <Route path="/settings" component={ Settings } />
+      <Route path="/game" component={ Game } />
     </Switch>
   );
 }
