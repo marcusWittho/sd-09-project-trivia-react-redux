dfbe0d43d6c7ead0bbc4d441252d1256191e1e82
ricardo-rals <ricardo.rals.dev@gmail.com>
2021-04-06 19:55:39 -0300

    requisito 4

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
 // import { Switch, Route } from 'react-router-dom';
-import logo from './trivia.png';
+// import logo from './trivia.png';
 import './App.css';
+import Header from './components/header';
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          SUA VEZ
-        </p>
-      </header>
-    </div>
+    <Header />
   );
 }
