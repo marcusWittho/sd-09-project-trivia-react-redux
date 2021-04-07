a09bb1eca819d1de6973216e19a7a35d57af0fc7
Caio Daniel Nunes Santos <caio.daniel.nunes.santos@gmail.com>
2021-04-07 16:29:53 -0300

    Login component barebones

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,9 +1,49 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+    };
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  }
+
   render() {
+    const { name, email } = this.state;
     return (
-      <p>Login Page</p>
+      <form>
+        <label htmlFor="name">
+          User Name
+          <input
+            type="text"
+            name="name"
+            data-testid="input-player-name"
+            id="name"
+            value={ name }
+            onChange={ this.handleChange.bind(this) }
+          />
+        </label>
+        <br />
+        <label htmlFor="email">
+          Gravatar Email
+          <input
+            type="text"
+            name="email"
+            data-testid="input-gravatar-email"
+            id="email"
+            value={ email }
+            onChange={ this.handleChange.bind(this) }
+          />
+        </label>
+        <br />
+        <button type="button">Começar a Jogar</button>
+      </form>
     );
   }
 }
