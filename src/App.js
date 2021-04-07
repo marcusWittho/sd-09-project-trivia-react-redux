6397648adbcc463b97204ab1c4ee3e2a3e51bf4f
wend3ll-souza <wend3llsouza@gmail.com>
2021-04-07 19:18:54 -0300

    :checkered_flag: Req 3 - Setting Screen

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import logo from './trivia.png';
 import Login from './pages/Login';
 import Game from './pages/Game';
+import Settings from './pages/Settings';
 
 export default function App() {
   return (
@@ -15,6 +16,7 @@ export default function App() {
         <Switch>
           <Route exact path="/" component={ Login } />
           <Route path="/game" component={ Game } />
+          <Route path="/settings" component={ Settings } />
         </Switch>
       </section>
     </main>
