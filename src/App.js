66367528222577a8764f9f6cd0a774b62272fb0b
Marconi <marconieng@gmail.com>
2021-04-07 11:14:30 -0300

    Adiciona requisito 3

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Play from './pages/Play';
+import Config from './pages/Config';
 
 class App extends React.Component {
   render() {
     return (
       <Switch>
+        <Route path="/config" component={ Config } />
         <Route path="/play" component={ Play } />
         <Route exact path="/" component={ Login } />
       </Switch>
