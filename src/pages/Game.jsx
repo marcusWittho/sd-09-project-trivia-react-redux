7ef6edabc1b64120603a190941d2b2a3f3a50926
Murilo <mub4si@gmail.com>
2021-04-07 17:55:31 -0300

    Req02: Implement Login Page

---
+++ src/pages/Game.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Game extends Component {
+  render() {
+    return (
+
+      <div>
+        <h1>JogaAE</h1>
+      </div>
+    );
+  }
+}
+
+export default Game;
