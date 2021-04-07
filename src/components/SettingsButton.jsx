8e05e04d5779822718c464293c61ecc92645296a
Pedro <ped.alm2709@gmail.com>
2021-04-07 16:08:02 -0300

    correçao de lint

--- src/components/SettingsButton.jsx
+++ src/components/SettingsButton.jsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 class SettingsButton extends React.Component {
   render() {
     return (
       <div>
         <Link to="./settings">
-          <button data-testid="btn-settings">Settings</button>
+          <button data-testid="btn-settings" type="button">
+            Settings
+          </button>
         </Link>
       </div>
     );
