c57724eca18e2dd45af694de1c1d04596d61362b
LeonardoJosedaSilveira <rpgleo.js@gmail.com>
2021-04-07 09:32:01 -0300

    services fetch component first try

---
+++ src/Services/fetchApi.js
@@ -0,0 +1,19 @@
+import md5 from 'crypto-js/md5';
+
+export async function fetchQuestResponse(nrQuestions, token) {
+  // eu usava o await na chamada da função mais vi a ana usando do endpooint entao coloquei aqui
+  const endpoint = await `https://opentdb.com/api.php?amount=${nrQuestions}&token=${token}`;
+
+  return fetch(endpoint)
+    .then((listQuestions) => listQuestions.json())
+    .then((listQuestions) => listQuestions.results);
+}
+
+// verificar "response_code":3, no fetch do token
+
+export async function fetchGravatar(userEmail) {
+  const hashCode = md5(userEmail).toString();
+  const endpoint = await `https://www.gravatar.com/avatar/${hashCode}`;
+
+  return endpoint;
+}
