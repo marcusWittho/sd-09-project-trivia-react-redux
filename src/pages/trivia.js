12c6e5a0d82a4b421a4377736a92b048d77abe78
Kevin Fraga <kevinciolac@gmail.com>
2021-04-07 02:25:53 -0300

    componente header pronto

--- src/pages/trivia.js
+++ src/pages/trivia.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from './header';
 
 class trivia extends React.Component {
   render() {
     return (
       <div className="App">
+        <Header profile="teste" player="teste" score={ 7 } />
         <h1>Trivia</h1>
       </div>
     );
