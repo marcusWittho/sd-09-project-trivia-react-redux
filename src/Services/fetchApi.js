8ab299323177b7ad9e3c2ff940c6251099c817c3
Leonardo Jose da Silveira <67020759+LeonardoJosedaSilveira@users.noreply.github.com>
2021-04-07 10:01:37 -0300

    Update fetchApi.js

--- src/Services/fetchApi.js
+++ src/Services/fetchApi.js
@@ -5,7 +5,7 @@ import md5 from 'crypto-js/md5';
 export const requestToken = async () => {
   try {
     const response = await fetch('https://opentdb.com/api_token.php?command=request');
-    const apiToken = await response.json()  
+    const apiToken = await response.json();
     return apiToken;
   } catch (error) {
     return Error(error);
