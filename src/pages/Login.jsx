c8eb28ab5e569524118583aa3ad10897f6476a6d
Matheus Monteiro <monteiromsh@gmail.com>
2021-04-07 15:41:48 -0300

    requisito 12 - componente criado

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import md5 from 'crypto-js/md5';
+import { userEmail, userName, userAvatar } from '../actions/index';
+import '../App.css';
+import logo from '../trivia.png';
 
 class Login extends React.Component {
   constructor(props) {
@@ -9,7 +15,9 @@ class Login extends React.Component {
       validated: true,
     };
     this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
     this.validateInputs = this.validateInputs.bind(this);
+    this.fetchGravata = this.fetchGravata.bind(this);
   }
 
   handleChange({ target }) {
@@ -18,6 +26,13 @@ class Login extends React.Component {
       [name]: value,
     });
     this.validateInputs();
+  }
+
+  handleClick(email, name) {
+    const { userEmailDispatcher, userNameDispatcher } = this.props;
+    userEmailDispatcher(email);
+    userNameDispatcher(name);
+    this.fetchGravata();
   }
 
   validateInputs() {
@@ -29,10 +44,22 @@ class Login extends React.Component {
     }
   }
 
+  fetchGravata() {
+    const { userAvatarDispatcher } = this.props;
+    const { email } = this.props;
+    const hashEmail = md5(email).toString();
+    const urlAvatar = `https://www.gravatar.com/avatar/${hashEmail}`;
+    userAvatarDispatcher(urlAvatar);
+  }
+
   render() {
-    const { validated } = this.state;
+    const { validated, email, name } = this.state;
     return (
-      <div>
+      <header className="App-header">
+        <img src={ logo } className="App-logo" alt="logo" />
+        <p>
+          SUA VEZ
+        </p>
         <form>
           <label htmlFor="name">
             Nome:
@@ -58,13 +85,29 @@ class Login extends React.Component {
             disabled={ validated }
             type="button"
             data-testid="btn-play"
+            onClick={ () => this.handleClick(email, name) }
           >
             Jogar
           </button>
         </form>
-      </div>
+      </header>
     );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  userEmailDispatcher: PropTypes.func,
+  userNameDispatcher: PropTypes.func,
+}.isRequired;
+
+const mapDispatchToProps = (dispatch) => ({
+  userEmailDispatcher: (email) => dispatch(userEmail(email)),
+  userNameDispatcher: (name) => dispatch(userName(name)),
+  userAvatarDispatcher: (avatar) => dispatch(userAvatar(avatar)),
+});
+
+const mapStatetoProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(mapStatetoProps, mapDispatchToProps)(Login);
