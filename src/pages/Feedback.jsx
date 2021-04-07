869e7b0b7656236d985bbbe89670a8789545542b
Matheus Martino <matheus.martino97@gmail.com>
2021-04-07 17:17:41 -0300

    Começando a desenvolver a pagina de feedback.

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,17 +1,38 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import md5 from 'crypto-js/md5';
+import PropTypes from 'prop-types';
 
 class Feedback extends Component {
   render() {
+    const { email } = this.props;
+    const cryptoEmail = md5(email).toString();
+    const { player } = JSON.parse(localStorage.getItem('state'));
+
     return (
       <div>
         <header>
-          <span data-testid="header-profile-picture"></span>
-          <span data-testid="header-player-name"></span>
-          <span data-testid="header-score"></span>
+          <span data-testid="header-profile-picture">
+            <img src={ `https://www.gravatar.com/avatar/${cryptoEmail}` } alt="profile" />
+          </span>
+          <span data-testid="header-player-name">
+            <h1>{ player.name }</h1>
+          </span>
+          <span data-testid="header-score">
+            <p>{ player.score }</p>
+          </span>
         </header>
       </div>
     );
   }
 }
 
-export default Feedback;
+const mapStateToProps = (state) => ({
+  email: state.loginReducer.email,
+});
+
+Feedback.propTypes = {
+  email: PropTypes.string,
+}.isRequired;
+
+export default connect(mapStateToProps)(Feedback);
