0185743e11540aa121de122fb2637980f9514fe4
jorgeramires <jorgerl123@gmail.com>
2021-04-07 18:02:00 -0300

    req03

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -74,6 +74,9 @@ class Login extends React.Component {
             JOGAR!
           </button>
         </Link>
+        <Link to="/Config">
+          <button type="button" data-testid="btn-settings">Configurações</button>
+        </Link>
       </section>
     );
   }
