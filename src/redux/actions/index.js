17494439392489ffc8aa326c6ce824e8273cd99f
BBrolesi <brunohsbrolesi@gmail.com>
2021-04-07 15:37:48 -0300

    feat: requirement 02

---
+++ src/redux/actions/index.js
@@ -0,0 +1,8 @@
+import { CLICK_PLAY } from './typeActions';
+
+const clickPlay = (state) => ({
+  type: CLICK_PLAY,
+  email: state,
+});
+
+export default clickPlay;
---
+++ src/redux/actions/typeActions.js
@@ -0,0 +1,3 @@
+export const CLICK_PLAY = 'CLICK_PLAY';
+
+export const OUTRA_ACTION = 'OUTRA_ACTION';
