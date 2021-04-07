3ac2c66c4a88454fd09a06f94be44de3eab23f4d
Henrique Brito Elias <henriquebelias@gmail.com>
2021-04-07 18:47:34 -0300

    main-group-18 -> requisito 3

---
+++ src/pages/Configuration.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Configuration extends React.Component {
+  render() {
+    return (
+      <h1 data-testid="settings-title">Página de Configuração</h1>
+    );
+  }
+}
+
+export default Configuration;
