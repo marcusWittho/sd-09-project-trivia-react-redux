46c2e609276328832c29e6f7a6fb9c8783491a54
RodrigoNeoEx <draja.acc@gmail.com>
2021-04-07 09:43:47 -0300

    lint

--- src/Components/SelectSettings.js
+++ src/Components/SelectSettings.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+// import { connect } from 'react-redux';
+// import PropTypes from 'prop-types';
 
 class SelectSettings extends Component {
   render() {
