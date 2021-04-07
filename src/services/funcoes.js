b21ac05644323fd3b0d46b1969829f94bfdcffaa
BernardoFontes <bfontes.1997@gmail.com>
2021-04-06 21:09:01 -0300

    req2

--- src/services/funcoes.js
+++
@@ -1,7 +0,0 @@
-const reqApi = () => {
-    fetch('https://opentdb.com/api_token.php?command=request')
-    .then((resposta) => resposta.json())
-    .then((token) => token)
-}
-
-export default reqApi;
