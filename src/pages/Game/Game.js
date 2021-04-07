6bfa1df53da607d5bcf5ba09688ce697933838ae
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2021-04-07 16:00:14 -0300

    iniciando requisito 10

--- src/pages/Game/Game.js
+++ src/pages/Game/Game.js
@@ -9,9 +9,27 @@ import Loading from '../../components/Loading/Loading';
 import './Game.css';
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showButton: false,
+    };
+    this.handleNextQuestion = this.handleNextQuestion.bind(this);
+  }
+
+  // Fazer a verificação, se a questão foi marcada então passe para a próxima
+  handleNextQuestion() {
+    const { questions } = this.props;
+    console.log(questions);
+    this.setState({
+      showButton: true,
+    });
+  }
+
   render() {
     const { player, questions, isFetching } = this.props;
     const { validLogin } = player;
+    const { showButton } = this.state;
     if (!validLogin) return <Redirect exact to="/" />;
     if (isFetching || !questions) return <Loading />;
     return (
@@ -32,6 +50,16 @@ class Game extends React.Component {
                 ? <MultipleAnswers question={ question } />
                 : <BooleanAnswers question={ question } />
             ))[0] }
+            {(showButton) && (
+              <button
+                type="button"
+                data-testid="btn-next"
+                onClick={ () => this.handleNextQuestion() }
+              >
+                Próxima
+              </button>
+            )}
+
           </div>
         </main>
       </section>
