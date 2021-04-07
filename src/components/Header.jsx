85a86f7251aa8fdf14076cceb4627d584f8d8a21
Micaela Alves <micaela.alves93@gmail.com>
2021-04-07 05:14:25 -0300

    Req 4 - associação dos elementos ao local storage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,36 @@
 import React, { Component } from 'react';
+import md5 from 'crypto-js/md5';
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+
+    this.getGravatar = this.getGravatar.bind(this);
+  }
+
+  getGravatar(email) {
+    const hash = md5(email).toString();
+    const emailHash = `https://www.gravatar.com/avatar/${hash}`;
+    localStorage.setItem('picture', emailHash);
+    return emailHash;
+  }
+
   render() {
+    const email = localStorage.getItem('gravatarEmail');
+    const playerName = localStorage.getItem('name');
+    const score = localStorage.getItem('score');
     return (
       <>
-        <img data-testid="header-profile-picture" src="" alt="" />
-        <h3 data-testid="header-player-name">Nome da Pessoa</h3>
-        <p data-testid="header-score">Placar</p>
+        <img
+          data-testid="header-profile-picture"
+          src={ this.getGravatar(email) }
+          alt="Imagem do Jogador"
+        />
+        <h3 data-testid="header-player-name">{ playerName }</h3>
+        <p data-testid="header-score">{ score }</p>
       </>
     );
   }
 }
 
-export default Header
+export default Header;
