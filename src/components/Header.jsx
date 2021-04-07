4d3e7ae45ae8b0c1b7c55bd623c9f2a192e62a2b
Micaela Alves <micaela.alves93@gmail.com>
2021-04-07 03:30:52 -0300

    Req 4 - add elementos requeridos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,11 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-      <h1>Header</h1>
+      <>
+        <img data-testid="header-profile-picture" src="" alt="" />
+        <h3 data-testid="header-player-name">Nome da Pessoa</h3>
+        <p data-testid="header-score">Placar</p>
+      </>
     );
   }
 }
