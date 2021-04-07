341a18b2f64cc52e502c36947e5e07c6015b69f1
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2021-04-07 20:45:26 -0300

    add rotas

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import { Switch, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import SettingsPage from './pages/Settings';
 import FeedbackPage from './pages/FeedbackPage';
-// import LoginPage from './components/Login';
+import LoginPage from './pages/Login';
 
 export default function App() {
   return (
     <Switch>
-      {/* <Route path="/" component={ LoginPage }/> */}
+      <Route path="/" component={ LoginPage } />
       <Route path="/main-page" component={ MainPage } />
       <Route path="/settings" component={ SettingsPage } />
       <Route path="/feedback" component={ FeedbackPage } />
