d5989f2243dd82f41eec152ffddb8feb9309ebe2
Osvaldo Borges <netoo70@gmail.com>
2021-04-07 16:06:10 -0300

    Requisito 10

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { disabledQuest, nextQuest } from '../actions/gameAction';
+// import { disabledQuest, nextQuest } from '../actions/gameAction';
 
 class Game extends React.Component {
   constructor(props) {
@@ -10,43 +10,68 @@ class Game extends React.Component {
     this.changeQuest = this.changeQuest.bind(this);
     this.createQuest = this.createQuest.bind(this);
     this.buttonQuest = this.buttonQuest.bind(this);
+    this.timeNumberFunc = this.timeNumberFunc.bind(this);
 
     this.state = {
       index: 0,
+      timeNumber: 30,
+      disabledQuest: false,
+      disabledButton: true,
     };
   }
 
-  changeQuest() {
-    const { setNextQuest } = this.props;
-    setNextQuest();
-    this.setState((prevState) => ({
-      index: prevState.index + 1,
-    }));
+  componentDidMount() {
+    this.timeNumberFunc();
   }
 
-  buttonQuest(timerButton) {
+  componentDidUpdate() {
+    const { timeNumber } = this.state;
+    if (timeNumber < 1) {
+      clearInterval(this.time);
+    }
+  }
+
+  changeQuest() {
+    this.setState((prevState) => ({
+      index: prevState.index + 1,
+      disabledQuest: false,
+      disabledButton: true,
+      timeNumber: 30,
+    }));
+    this.timeNumberFunc();
+  }
+
+  timeNumberFunc() {
+    const timerInterval = 1000;
+
+    this.time = setInterval(() => {
+      this.setState((prevState) => ({
+        timeNumber: prevState.timeNumber - 1,
+      }));
+    }, timerInterval);
+  }
+
+  buttonQuest() {
     const buttonC = document.querySelector('.button-correct');
     buttonC.style.border = '3px solid rgb(6, 240, 15)';
 
     const buttonIC = document.querySelectorAll('.button-incorrect');
     buttonIC.forEach((button) => { button.style.border = '3px solid rgb(255, 0, 0)'; });
 
-    const { setDisabledQuest } = this.props;
-    clearTimeout(timerButton);
-    setDisabledQuest();
+    clearInterval(this.time);
+    this.setState({
+      disabledQuest: true,
+      disabledButton: false,
+    });
   }
 
   createQuest(index) {
-    const { questions, getDisableQuest, setDisabledQuest } = this.props;
+    const { questions } = this.props;
+    const { disabledQuest, timeNumber } = this.state;
 
     const t = [...questions[index].incorrect_answers, questions[index].correct_answer];
     // const random = 0.5;
     // t = t.sort(() => Math.random() - random);
-
-    const timeNumber = 30000;
-    const timerButton = setTimeout(() => {
-      setDisabledQuest();
-    }, timeNumber);
 
     return (
       <div>
@@ -57,8 +82,8 @@ class Game extends React.Component {
                 type="button"
                 className="button-correct"
                 key={ response }
-                onClick={ () => this.buttonQuest(timerButton) }
-                disabled={ getDisableQuest }
+                onClick={ this.buttonQuest }
+                disabled={ disabledQuest || timeNumber < 1 }
                 data-testid="correct-answer"
               >
                 {response}
@@ -69,8 +94,8 @@ class Game extends React.Component {
               type="button"
               className="button-incorrect"
               key={ response }
-              onClick={ () => this.buttonQuest(timerButton) }
-              disabled={ getDisableQuest }
+              onClick={ this.buttonQuest }
+              disabled={ disabledQuest || timeNumber < 1 }
               data-testid={ `wrong-answer-${indexRep}` }
             >
               {response}
@@ -82,7 +107,7 @@ class Game extends React.Component {
 
   render() {
     const { questions, loading } = this.props;
-    const { index } = this.state;
+    const { index, disabledButton, timeNumber } = this.state;
 
     if (loading) {
       return (<p>Loading...</p>);
@@ -95,6 +120,8 @@ class Game extends React.Component {
         { this.createQuest(index) }
         <button
           type="button"
+          hidden={ disabledButton && timeNumber !== 0 }
+          data-testid="btn-next"
           onClick={ this.changeQuest }
         >
           Proxima
@@ -107,20 +134,11 @@ class Game extends React.Component {
 const mapStateToProps = (state) => ({
   questions: state.gameReducer.questions,
   loading: state.gameReducer.loading,
-  getDisableQuest: state.gameReducer.disabledQuest,
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  setDisabledQuest: () => dispatch(disabledQuest()),
-  setNextQuest: () => dispatch(nextQuest()),
 });
 
 Game.propTypes = {
   questions: PropTypes.arrayOf(PropTypes.object).isRequired,
   loading: PropTypes.bool.isRequired,
-  setDisabledQuest: PropTypes.func.isRequired,
-  getDisableQuest: PropTypes.bool.isRequired,
-  setNextQuest: PropTypes.func.isRequired,
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Game);
+export default connect(mapStateToProps)(Game);
