bba72662de1a9300979959b00666838e315c7d1e
smylle <jjsilveira007@gmail.com>
2021-04-07 17:44:03 -0300

    Cria componente Header e pagina Feedback

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React  from 'react';
+
+class Header extends React.Component {
+    render() {
+        return (
+            <header>
+                <img src="https://via.placeholder.com/80" alt="avatar" data-testid="header-profile-picture"/>
+                <p data-testid="header-player-name">nome</p>
+                <p data-testid="header-score">placar</p>
+            </header>
+        )
+    }
+}
