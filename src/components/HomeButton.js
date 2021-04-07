2764367afe59f4b7be7d96479fe9276c695807e1
Marina Alane <marina.alane@outlook.com>
2021-04-07 18:53:12 -0300

    fix requirement

--- src/components/HomeButton.js
+++ src/components/HomeButton.js
@@ -1,19 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-class HomeButton extends React.Component {
+class Ranking extends Component {
   render() {
     return (
-      <Link to="/">
-        <button
-          type="button"
-          data-testid="btn-go-home"
-        >
-          Home
-        </button>
-      </Link>
+      <section>
+        <title data-testid="ranking-title">Ranking</title>
+        <Link to="/">
+          <button
+            type="button"
+            data-testid="btn-go-home"
+          >
+            Home
+          </button>
+        </Link>
+      </section>
+
     );
   }
 }
 
-export default HomeButton;
+export default Ranking;
