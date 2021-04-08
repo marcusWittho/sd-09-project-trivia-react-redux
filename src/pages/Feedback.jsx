8d989cdc7169181fa19d1da8dc7b3dc6d9888696
Leticia Lima <letylima96@gmail.com>
2021-04-07 22:28:17 -0300

    adiciona componente FeedbackMsg para a mensagem de feedback

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../components/Header';
+import FeedbackMsg from '../components/FeedbackMsg';
 
 class Feedback extends React.Component {
   render() {
     return (
       <div>
         <Header />
+        <FeedbackMsg />
       </div>
     );
   }
