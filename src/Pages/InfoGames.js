bd77a556adc25f5c0a261b19cb7b0c47ed15ff15
MarcioFujita <hanker78@gmail.com>
2021-04-08 16:12:59 -0300

    Requisito 5 v1

--- src/Pages/InfoGames.js
+++ src/Pages/InfoGames.js
@@ -1,12 +1,73 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import * as api from '../services/fetchApi';
 
 class InfoGames extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      questions: [],
+      indice: 0,
+      isLoading: true,
+    };
+    this.requestAPI = this.requestAPI.bind(this);
+    this.renderQuestions = this.renderQuestions.bind(this);
+  }
+
+  componentDidMount() {
+    this.requestAPI();
+  }
+
+  async requestAPI() {
+    const token = JSON.parse(localStorage.getItem('token'));
+    const quantitiQuestions = 5;
+    const responsRequest = await api.fetchTrivia(token, quantitiQuestions);
+    this.setState({
+      questions: responsRequest.results,
+      isLoading: false,
+    });
+  }
+
+  renderQuestions() {
+    const { questions, indice } = this.state;
+    const crrQuestion = questions[indice];
     return (
-      <Header />
+      <div>
+        <Header />
+        <h1 data-testid="question-category">
+          {crrQuestion.category}
+        </h1>
+        <h2 data-testid="question-text">{crrQuestion.question}</h2>
+        <button
+          type="button"
+          onClick={
+            () => this.setState((prevState) => ({ indice: prevState.indice + 1 }))
+          }
+        >
+          <p data-testid="correct-answer">{crrQuestion.correct_answer}</p>
+        </button>
+        {crrQuestion.incorrect_answers.map((incorrectAnswer, index) => (
+          <button
+            type="button"
+            key={ Math.random() }
+            onClick={
+              () => this.setState((prevState) => ({ indice: prevState.indice + 1 }))
+            }
+          >
+            <p data-testid={ `wrong-answer-${index}` } key={ Math.random() }>
+              {incorrectAnswer}
+            </p>
+          </button>
+        ))}
+      </div>
+    );
+  }
+
+  render() {
+    const { isLoading } = this.state;
+    return (
+      isLoading ? <p>Loading...</p> : this.renderQuestions()
     );
   }
 }
-
 export default InfoGames;
