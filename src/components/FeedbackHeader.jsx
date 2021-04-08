5d9fa7c57efda374c9edd2f00e088f92cae67e77
Carine Becker <caarine.m@gmail.com>
2021-04-08 11:46:54 -0300

    renderiza header

--- src/components/FeedbackHeader.jsx
+++ src/components/FeedbackHeader.jsx
@@ -3,19 +3,21 @@ import React, { Component } from 'react';
 export default class FeedbackHeader extends Component {
   render() {
     return (
-      <header>
-        <img
-          data-testid="header-profile-picture"
-          alt=""
-          src=""
-          className="header-profile-picture"
-        />
-        <span
-          data-testid="header-player-name"
-          className="header-player-name"
-        >
-          Nome jogador
-        </span>
+      <header className="feedback-header">
+        <div className="header-left">
+          <img
+            data-testid="header-profile-picture"
+            alt=""
+            src=""
+            className="header-profile-picture"
+          />
+          <span
+            data-testid="header-player-name"
+            className="header-player-name"
+          >
+            Nome jogador
+          </span>
+        </div>
         <span
           data-testid="header-score"
           className="header-score"
