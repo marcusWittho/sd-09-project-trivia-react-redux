edc2d72f1437e928bde83faba4088b8964491a87
Kamilavrhcp <kamilavrhcp@hotmail.com>
2021-04-08 16:10:27 -0300

    Feat: requisitos 12, 13 e 14

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import Header from '../components/Header';
 
 export default class Feedback extends React.Component {
   render() {
+    const userData = JSON.parse(localStorage.getItem('ranking'));
+    const { score } = userData;
+    const numberScore = 3;
+    const totalScore = 10;
     return (
-      <h1>Boa sorte!</h1>
+      <div>
+        <Header />
+        <p data-testid="correct-answer"> </p>
+        <p data-testid="feedback-text">
+          { score >= numberScore ? 'Mandou bem!' : 'Podia ser melhor...' }
+        </p>
+        <p>
+          Você acertou
+          {' '}
+          <span data-testid="feedback-total-score">
+            { score }
+          </span>
+          {' '}
+          questões!
+        </p>
+        <p>
+          Um total de
+          {' '}
+          <span data-testid="feedback-total-question">
+            { score * totalScore }
+          </span>
+          {' '}
+          pontos.
+        </p>
+      </div>
     );
   }
 }
