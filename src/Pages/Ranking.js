596031025eafb1236ac9d276d92b888ebb8d94b6
Tiago de Almeida Paz <tiagopaz.dev@gmail.com>
2021-04-08 13:53:28 -0300

    group-29/feat-17/18

--- src/Pages/Ranking.js
+++ src/Pages/Ranking.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Ranking extends Component {
   render() {
+    const ranking = JSON.parse(localStorage.getItem('ranking'));
+    console.log(ranking);
     return (
       <div>
         <h2 data-testid="ranking-title">
           Ranking
         </h2>
+        <ul>
+          { ranking.map(({ name, score, picture }, index) => (
+            <li key={ name }>
+              <img src={ picture } alt="User Avatar" />
+              <p data-testid={ `player-name-${index}` }>{ name }</p>
+              <p data-testid={ `player-score-${index}` }>{ score }</p>
+            </li>
+          ))}
+        </ul>
+        <button type="button" data-testid="btn-go-home">
+          <Link to="/">
+            Home
+          </Link>
+        </button>
       </div>
     );
   }
