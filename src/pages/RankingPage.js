e9b824fb0528833925c05d27b869ab87c56bc406
Maximilian Kaden <mkadenfilgo@gmail.com>
2021-04-08 18:53:36 -0300

    Cria tabela com participantes pelo LocalStorage

--- src/pages/RankingPage.js
+++ src/pages/RankingPage.js
@@ -1,29 +1,60 @@
 import React from 'react';
+import '../styles/RankingPage.css';
 
 const rankingTest = [
-  { name: 'nome-da-pessoa 1', score: 10, picture: 'url-da-foto-no-gravatar' },
-  { name: 'nome-da-pessoa 2', score: 20, picture: 'url-da-foto-no-gravatar' },
+  { name: 'nome-da-pessoa 1', score: 30, picture: 'https://www.gravatar.com/avatar/9e34f9c6d60b08c3049a260331c827f9' },
+  { name: 'nome-da-pessoa 2', score: 20, picture: 'https://www.gravatar.com/avatar/9e34f9c6d60b08c3049a260331c827f9' },
 ];
 
 class RankingPage extends React.Component {
+  getRankingOrderedByScore() {
+    const ranking = JSON.parse(localStorage.getItem('ranking'));
+    ranking.sort((a, b) => {
+      if (a.score < b.score) {
+        const minus1 = -1;
+        return minus1;
+      }
+      if (a.score > b.score) {
+        return 1;
+      }
+      return 0;
+    });
+    return ranking;
+  }
+
   createParticipantList(participant, index) {
+    this.getRankingOrderedByScore();
     const { name, score, picture } = participant;
     return (
-      <li>
-        <img src={ picture } alt="user" />
-        <p data-testid={ `player-name-${index}` }>{name}</p>
-        <p data-testid={ `player-score-${index}` }>{score}</p>
-      </li>
+      <tr className="ranking-list">
+        <td>
+          <img src={ picture } alt="user" />
+        </td>
+        <td>
+          <p data-testid={ `player-name-${index}` }>{name}</p>
+        </td>
+        <td>
+          <p data-testid={ `player-score-${index}` }>{score}</p>
+        </td>
+      </tr>
     );
   }
 
   render() {
+    localStorage.setItem('ranking', JSON.stringify(rankingTest));
     return (
-      <ul>
-        {rankingTest.map(
-          (participant, index) => this.createParticipantList(participant, index),
-        )}
-      </ul>
+      <div>
+        <table>
+          <tr>
+            <th>Imagem</th>
+            <th>Nome do participante</th>
+            <th>Pontuação</th>
+          </tr>
+          {this.getRankingOrderedByScore().map(
+            (participant, index) => this.createParticipantList(participant, index),
+          )}
+        </table>
+      </div>
     );
   }
 }
