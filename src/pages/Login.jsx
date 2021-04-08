0033b031abf0b4869dde179afed717543b6d8a54
vinicius <viniciuspalenske@gmail.com>
2021-04-08 10:34:14 -0300

    entrega do requisito 1 - tela de login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,50 +1,58 @@
 import React from 'react';
-import { func } from 'prop-types';
-// import { handleEmail } from '../actions';
+// import { func } from 'prop-types';
 import './css/login.css';
 
 class Login extends React.Component {
-  // handleValidateLogin() {
-  //   const { email, password } = this.state;
-  //   const validateEmail = /^[\w.]+@[a-z]+\.\w{2,3}$/g;
-  //   const passwordLength = password.length;
-  //   const minPasswordLength = 1;
-  //   if (validateEmail.test(email) && passwordLength >= minPasswordLength) {
-  //     this.setState({ desactivatedButton: false });
-  //   } else {
-  //     this.setState({ desactivatedButton: true });
-  //   }
-  // }
+  constructor() {
+    super();
+    this.state = {
+      user: '',
+      email: '',
+      disableButton: true,
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target: { value, name } }) {
+    this.setState({ [name]: value }, () => {
+      const { user, email } = this.state;
+      const regex = /\S+@\S+\.\S+/;
+      const min = 0;
+      if (regex.test(email) && user.length > min) {
+        this.setState({ disableButton: false });
+      } else this.setState({ disableButton: true });
+    });
+  }
 
   render() {
+    const { disableButton } = this.state;
     return (
       <div className="form-login">
-        <h2 className="title">Login</h2>
         <form className="fieldset-login">
-          <label htmlFor="email-input" className="label-login">
+          <label htmlFor="enter-name">
+            Nome:
+            <input
+              id="enter-name"
+              data-testid="input-player-name"
+              type="text"
+              name="user"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <label htmlFor="enter-email" className="label-login">
             E-mail:
             <input
-              id="email-input"
+              id="enter-email"
               data-testid="input-gravatar-email"
               type="email"
               name="email"
-              // onChange={ ({ target }) => this.handleInputChange(target) }
-            />
-          </label>
-          <label htmlFor="input-player-name">
-            Nome:
-            <input
-              id="input-player-name"
-              data-testid="name-input"
-              type="text"
-              name="name-input"
-              // onChange={ ({ target }) => this.handleInputChange(target) }
+              onChange={ this.handleChange }
             />
           </label>
           <button
             type="button"
             data-testid="btn-play"
-            // disabled={ desactivatedButton }
+            disabled={ disableButton }
             // onClick={ this.redirect }
           >
             Jogar
@@ -55,8 +63,8 @@ class Login extends React.Component {
   }
 }
 
-Login.propTypes = {
-  emailDispatcher: func,
-}.isRequired;
+// Login.propTypes = {
+//   emailDispatcher: func,
+// }.isRequired;
 
 export default Login;
