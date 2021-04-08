bafc9fb3c26431f0c943e7ef93c3bc4fc59018fe
Marina Alane <marina.alane@outlook.com>
2021-04-08 17:23:27 -0300

    Adc componente header

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import HomeButton from './HomeButton';
+import Header from '../Componentes/Header';
 
 class Feedback extends React.Component {
   render() {
-    const url = 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50';
     return (
       <div>
-        <header>
-          <img src={ url } alt="imagem avatar" data-testid="header-profile-picture" />
-          <span data-testid="header-player-name">{ player.nome }</span>
-          <span data-testid="header-score">{ player.score }</span>
-        </header>
-        <HomeButton />
+        <Header />
       </div>
     );
   }
