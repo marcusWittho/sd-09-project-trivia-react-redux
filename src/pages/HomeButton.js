872c4550be905194ab4a04ee9f62a3bda5d3d85a
Marina Alane <marina.alane@outlook.com>
2021-04-08 13:34:13 -0300

    implementando sugestao do colega Giovani

--- src/pages/HomeButton.js
+++ src/pages/HomeButton.js
@@ -4,18 +4,14 @@ import { Link } from 'react-router-dom';
 class HomeButton extends Component {
   render() {
     return (
-      <section>
-        <button
-          type="button"
-          data-testid="btn-go-home"
-        >
-          <Link to="/">
-            Home
-          </Link>
-        </button>
-
-      </section>
-
+      <button
+        type="button"
+        data-testid="btn-go-home"
+      >
+        <Link to="/">
+          Home
+        </Link>
+      </button>
     );
   }
 }
