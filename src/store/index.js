4bfe152b11adc5f6ec922ef393f6739c841c4919
Marcelo Campos <31359152+marcelocampos66@users.noreply.github.com>
2021-04-07 21:26:46 -0300

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -2,9 +2,9 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 
-const extension = window
-  .__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const extension = (window
+  .__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()) || ((f) => f);
 
 const store = createStore(rootReducer, compose(applyMiddleware(thunk), extension));
 
-export default store;
+export default store
