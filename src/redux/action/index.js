99b753b7273968c330058f8f411af3aed44dc9e4
marcusWittho <witthoeft.mcs@hotmail.com>
2021-04-08 15:36:46 -0300

    Change branch and merge fix

--- src/redux/action/index.js
+++ src/redux/action/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const GET_TOKEN_API_BASE = 'https://opentdb.com/api_';
 
 const getToken = () => (
@@ -12,26 +11,3 @@ const getToken = () => (
 );
 
 export default getToken;
-=======
-import fetchApiToken from '../../services';
-
-export const REQUEST_TOKEN = 'REQUEST_TOKEN';
-export const RECIVE_TOKEN = 'RECIVE_TOKEN';
-
-const reciveToken = (token) => ({
-  type: RECIVE_TOKEN,
-  token,
-});
-
-const requestToken = () => ({
-  type: REQUEST_TOKEN,
-});
-
-export function fetchAPI() {
-  return async (dispatch) => {
-    dispatch(requestToken());
-    const response = await fetchApiToken();
-    return dispatch(reciveToken(response));
-  };
-}
->>>>>>> f2cd80e65a9d3494aa763a8566d71d54a91b7a89
