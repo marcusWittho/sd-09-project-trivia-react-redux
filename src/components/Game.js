24b15fe8dca02976074f07ec47a2852c9bd814b8
Marina Alane <marina.alane@outlook.com>
2021-04-08 10:18:03 -0300

    homebutton ok

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import HomeButton from './HomeButton';
 
 class Game extends React.Component {
   render() {
     return (
-      <div>Game</div>
+      <div>
+        Game
+        <HomeButton />
+      </div>
     );
   }
 }
