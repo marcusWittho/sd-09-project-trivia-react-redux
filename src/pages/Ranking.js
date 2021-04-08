ab13e404ea86cb1cfcdd0ee4f3eee198644592e5
Layo Kaminski <layokaminski@gmail.com>
2021-04-08 17:49:29 -0300

    Requerimento 14, 15 e 16

---
+++ src/pages/Ranking.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Ranking extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3 data-testid="ranking-title">Ranking</h3>
+      </div>
+    );
+  }
+}
+
+export default Ranking;
