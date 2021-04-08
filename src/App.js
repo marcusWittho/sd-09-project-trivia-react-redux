ade791d66dc35685db0e0160400ba6c092d9d33a
giovani.diccini <giovani-diccini@pop-os.localdomain>
2021-04-08 16:15:51 -0300

    fix conflicts2

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
-import GameScreen from './pages/GameScreen';
+import Game from './pages/Game';
 
 export default function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
-      <Route exact path="/game" component={ GameScreen } />
+      <Route exact path="/game" component={ Game } />
     </Switch>
   );
 }
