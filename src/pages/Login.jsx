e8df973044e9505a5cb36859bae165c8fa376a75
Matheus Martino <matheus.martino97@gmail.com>
2021-04-08 15:46:01 -0300

    Fixing lint issues.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -15,6 +15,7 @@ class Login extends Component {
     };
 
     this.handleChange = this.handleChange.bind(this);
+    this.returnForm = this.returnForm.bind(this);
   }
 
   handleChange({ target }) {
@@ -23,65 +24,70 @@ class Login extends Component {
     });
   }
 
-  // eslint-disable-next-line max-lines-per-function
+  returnForm(name, email, getToken) {
+    return (
+      <form action="">
+        <label htmlFor="input-player-name">
+          Nome:
+          <input
+            onChange={ this.handleChange }
+            data-testid="input-player-name"
+            type="text"
+            name="name"
+            value={ name }
+            id="input-player-name"
+          />
+        </label>
+        <label htmlFor="input-gravatar-email">
+          Email:
+          <input
+            onChange={ this.handleChange }
+            data-testid="input-gravatar-email"
+            type="email"
+            name="email"
+            value={ email }
+            id="input-gravatar-email"
+          />
+        </label>
+        <Link to="/game">
+          <button
+            onClick={() => {
+              const stateKey = {
+                player: {
+                  name,
+                  assertions: 0,
+                  score: 0,
+                  gravatarEmail: email,
+                },
+              };
+
+              localStorage.setItem('state', JSON.stringify(stateKey));
+              getToken({ name, email });
+            }}
+            data-testid="btn-play"
+            type="button"
+            disabled={ !name || !email }
+          >
+            Jogar
+          </button>
+        </Link>
+      </form>
+    );
+  }
+
   render() {
     const { name, email } = this.state;
     const { getToken } = this.props;
 
     return (
       <div>
-        <form action="">
-          <label htmlFor="input-player-name">
-            Nome:
-            <input
-              onChange={ this.handleChange }
-              data-testid="input-player-name"
-              type="text"
-              name="name"
-              value={ name }
-              id="input-player-name"
-            />
-          </label>
-          <label htmlFor="input-gravatar-email">
-            Email:
-            <input
-              onChange={ this.handleChange }
-              data-testid="input-gravatar-email"
-              type="email"
-              name="email"
-              value={ email }
-              id="input-gravatar-email"
-            />
-          </label>
-          <Link to="/game">
-            <button
-              onClick={ () => {
-                const stateKey = {
-                  player: {
-                    name,
-                    assertions: 0,
-                    score: 0,
-                    gravatarEmail: email,
-                  },
-                };
-
-                localStorage.setItem('state', JSON.stringify(stateKey));
-
-                getToken({ name, email });
-              } }
-              data-testid="btn-play"
-              type="button"
-              disabled={ !name || !email }
-            >
-              Jogar
-            </button>
-          </Link>
-        </form>
+        {this.returnForm(name, email, getToken)}
         <Link to="/settings">
-          <button data-testid="btn-settings" type="button">Configurações</button>
+          <button data-testid="btn-settings" type="button">
+            Configurações
+          </button>
         </Link>
       </div>
-
     );
   }
 }
