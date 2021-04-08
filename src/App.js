9ef0b87d2c5c4ee93cead7cdfd16c5dac89dfd4d
giovani.diccini <giovani-diccini@pop-os.localdomain>
2021-04-08 15:53:34 -0300

    fix conflicts

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
-// import Game from './pages/Game';
 import GameScreen from './pages/GameScreen';
 
 export default function App() {
