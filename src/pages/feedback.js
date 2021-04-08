c2bc19ffc88b0cbc41a8f23903ec85b11da3d4fb
santiago <felipesantiagolds@gmail.com>
2021-04-08 17:34:47 -0300

    12 requisito

---
+++ src/pages/feedback.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../components/header';
+
+class Feedback extends React.Component {
+  render() {
+    return (
+      <Header />
+    );
+  }
+}
+
+export default Feedback;
