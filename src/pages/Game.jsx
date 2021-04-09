b3ea28a54320b6e4c08d350c6fdffcd9b8f92b44
Enio Nicael <enionicael@gmail.com>
2021-04-08 21:40:49 -0300

    Adicao das informacoes referentes a pergunta.

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,13 +1,56 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+// import { connect } from 'react-redux';
+// import PropTypes from 'prop-types';
+// import getQuestions from '../services/triviaAPI_questions';
 
-import getQuestions from '../services/triviaAPI_questions';
+import './Game.css';
 
-export default class Game extends Component {
+class Game extends Component {
   render() {
-    getQuestions();
+    // getQuestions();
     return (
-      <Header />
+      <>
+        <Header />
+        <div className="container-game">
+          <section className="question-game">
+            {/* campo category */}
+            <h2 data-testid="question-category">Politica</h2>
+            {/* campo question */}
+            <p data-testid="question-text">Texto da pergunta</p>
+          </section>
+          <section className="answers-game">
+
+          </section>
+        </div>
+      </>
     );
   }
 }
+
+const mapStateToProps = (state) => ({
+
+})
+
+export default Game;
+// export default connect(mapStateToProps)(Game);
+
+
+// Pergunta de múltipla escolha
+/* {
+  "response_code":0,
+  "results":[
+     {
+        "category":"Entertainment: Video Games",
+        "type":"multiple",
+        "difficulty":"easy",
+        "question":"What is the first weapon you acquire in Half-Life?",
+        "correct_answer":"A crowbar",
+        "incorrect_answers":[
+           "A pistol",
+           "The H.E.V suit",
+           "Your fists"
+        ]
+     }
+  ]
+} */
