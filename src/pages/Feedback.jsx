c1e8b623f8af3e5e158f7d1f1612d0c69f2931d6
Micaela Alves <micaela.alves93@gmail.com>
2021-04-09 20:49:19 -0300

    link de feedback para ranking

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 
 class FeedBack extends Component {
@@ -11,6 +12,14 @@ class FeedBack extends Component {
     return (
       <section>
         <Header />
+        <Link to="/ranking">
+          <button
+            type="button"
+            data-testid="btn-ranking"
+          >
+            Ver Ranking
+          </button>
+        </Link>
         {numberOfAssertions < condicionNumber
           ? <p data-testid="feedback-text">Podia ser melhor...</p>
           : <p data-testid="feedback-text">Mandou bem!</p>}
