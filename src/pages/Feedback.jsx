d8a3489c50e5313a5905819672eec4f2954c33ac
Henrique Brito Elias <henriquebelias@gmail.com>
2021-04-09 16:23:14 -0300

    main-group-18 -> requisitos 14 e 15

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
@@ -7,13 +8,24 @@ const MAGIC_NUMBER = 3;
 
 class Feedback extends React.Component {
   render() {
-    const { assertions } = this.props;
+    const { assertions, score } = this.props;
     return (
       <>
         <Header />
         <h3 data-testid="feedback-text">
           { assertions >= MAGIC_NUMBER ? 'Mandou bem!' : 'Podia ser melhor...' }
         </h3>
+        <h4>
+          Placar total:
+          <span data-testid="feedback-total-score">{score}</span>
+        </h4>
+        <h4>
+          Você acertou:
+          <span data-testid="feedback-total-question">{assertions}</span>
+        </h4>
+        <Link to="/">
+          <button type="button" data-testid="btn-play-again">Jogar novamente</button>
+        </Link>
       </>
     );
   }
@@ -21,10 +33,12 @@ class Feedback extends React.Component {
 
 const mapStateToProps = (state) => ({
   assertions: state.scoreReducer.assertions,
+  score: state.scoreReducer.score,
 });
 
 Feedback.propTypes = {
   assertions: PropTypes.number.isRequired,
+  score: PropTypes.number.isRequired,
 };
 
 export default connect(mapStateToProps)(Feedback);
