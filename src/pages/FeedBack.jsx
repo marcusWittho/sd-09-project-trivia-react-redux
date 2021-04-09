72213b755c5ce09e45dc74829635b8632caa9432
Pedro Henrique <pedro.876.ph@gmail.com>
2021-04-09 16:31:16 -0300

    requisito 15 e 16 :bomb:

--- src/pages/FeedBack.jsx
+++ src/pages/FeedBack.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 
 class FeedBack extends React.Component {
@@ -11,6 +12,8 @@ class FeedBack extends React.Component {
     this.getLocalStorage = this.getLocalStorage.bind(this);
     this.verifyAssertions = this.verifyAssertions.bind(this);
     this.scoreBoardGenerator = this.scoreBoardGenerator.bind(this);
+    this.playAgain = this.playAgain.bind(this);
+    this.rankingButton = this.rankingButton.bind(this);
   }
 
   componentDidMount() {
@@ -41,8 +44,6 @@ class FeedBack extends React.Component {
 
   scoreBoardGenerator() {
     const { correctAnswers, totalScore } = this.state;
-    console.log(correctAnswers);
-    console.log(totalScore);
     return (
       <div>
         <p data-testid="feedback-total-score">
@@ -55,6 +56,32 @@ class FeedBack extends React.Component {
     );
   }
 
+  playAgain() {
+    return (
+      <Link to="/">
+        <button
+          type="button"
+          data-testid="btn-play-again"
+        >
+          Jogar novamente
+        </button>
+      </Link>
+    );
+  }
+
+  rankingButton() {
+    return (
+      <Link to="/ranking">
+        <button
+          type="button"
+          data-testid="btn-ranking"
+        >
+          Ver Ranking
+        </button>
+      </Link>
+    );
+  }
+
   render() {
     return (
       <div>
@@ -62,6 +89,8 @@ class FeedBack extends React.Component {
         <Header />
         { this.verifyAssertions() }
         { this.scoreBoardGenerator() }
+        { this.playAgain() }
+        { this.rankingButton() }
       </div>
     );
   }
