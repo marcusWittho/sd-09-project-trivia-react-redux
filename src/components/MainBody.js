f0e22f91c549c6f4fa750889a92b5a904f6e2e87
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2021-04-09 17:16:26 -0300

    Requisito 6 ok

--- src/components/MainBody.js
+++ src/components/MainBody.js
@@ -4,33 +4,66 @@ import { string, objectOf } from 'prop-types';
 import { fetchQuestions } from '../actions';
 
 class MainBody extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      getQuestions: true,
+      category: '',
+      question: '',
+      correctAnswer: '',
+      incorrectAnswers: '',
+    };
+  }
+
+  componentDidUpdate() {
+    const { questions, loading } = this.props;
+    const { getQuestions } = this.state;
+    if (getQuestions) {
+      this.setQuestionsToEstate();
+    }
+  }
+
+  setQuestionsToEstate() {
+    const { questions: { results } } = this.props;
+    this.setState({
+      getQuestions: false,
+      category: results[0].category,
+      question: results[0].question,
+      correctAnswer: results[0].correct_answer,
+      incorrectAnswers: results[0].incorrect_answers,
+    });
+  }
+
+  renderQuestion(question) {
+    
+  }
+
   render() {
-    const { loading, questions } = this.props;
+    const { loading } = this.props;
+    const { category, question, correctAnswer, incorrectAnswers } = this.state;
     if (loading) {
       return <p>Loading...</p>;
     }
     return (
+      // <div></div>
       <div>
-        {questions.results.map((question, index) => (
-          <div key={ index }>
-            <p data-testid="question-category">{ question.category }</p>
-            <p data-testid="question-text">{ question.question }</p>
-            <button
-              type="button"
-              data-testid="correct-answer"
-            >
-              { question.correct_answer }
-            </button>
-            { question.incorrect_answers.map((incorrectAnswer) => (
-              <button
-                type="button"
-                key={ incorrectAnswer }
-                data-testid="wrong-answer"
-              >
-                { incorrectAnswer }
-              </button>
-            ))}
-          </div>
+        <p data-testid="question-category">{ category }</p>
+        <p data-testid="question-text">{ question }</p>
+        <button
+          type="button"
+          data-testid="correct-answer"
+        >
+          { correctAnswer }
+        </button>
+        {/* { console.log(incorrectAnswers)} */}
+        {incorrectAnswers && incorrectAnswers.map((incorrectAnswer) => (
+          <button
+            type="button"
+            key={ incorrectAnswer }
+            data-testid="wrong-answer"
+          >
+            { incorrectAnswer }
+          </button>
         ))}
       </div>
     );
