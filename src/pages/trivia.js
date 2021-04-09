74402c4a18f308cd66248a68f5cf93e276312ece
Kevin Fraga <kevinciolac@gmail.com>
2021-04-09 00:08:37 -0300

    btn-next hidden while not answered

--- src/pages/trivia.js
+++ src/pages/trivia.js
@@ -11,9 +11,15 @@ class trivia extends React.Component {
     this.state = {
       index: 0,
       loading: true,
+      answered: false,
     };
     this.handleClick = this.handleClick.bind(this);
     this.handleGetToken = this.handleGetToken.bind(this);
+    this.handleAnswer = this.handleAnswer.bind(this);
+  }
+
+  handleAnswer() {
+    this.setState({ answered: true });
   }
 
   handleGetToken() {
@@ -26,26 +32,31 @@ class trivia extends React.Component {
     const maxIndex = 4;
     const { index } = this.state;
     if (index === maxIndex) {
-      this.setState((previousState) => ({ index: previousState.index }));
+      this.setState((previousState) => ({ index: previousState.index, answered: false }));
     } else {
-      this.setState((previousState) => ({ index: previousState.index + 1 }));
+      this.setState((previousState) => ({
+        index: previousState.index + 1,
+        answered: false,
+      }));
     }
   }
 
   render() {
     const { results } = this.props;
-    const { index, loading } = this.state;
+    const { index, loading, answered } = this.state;
     const question = results.find((_question, i) => i === index);
     if (loading) this.handleGetToken();
     return (
       <div className="App">
         <Header />
         <h1>Trivia</h1>
-        {(!loading) && <Question question={ question } />}
+        {(!loading)
+        && <Question question={ question } handleAnswer={ this.handleAnswer } />}
         <button
           data-testid="btn-next"
           type="button"
           onClick={ this.handleClick }
+          hidden={ !answered }
         >
           Próxima
         </button>
