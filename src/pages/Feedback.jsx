cd46f103bb7a1f6f8dc1b2ec1da92fe0184186af
Henrique Brito Elias <henriquebelias@gmail.com>
2021-04-09 16:12:23 -0300

    main-group-18 -> requisito 13

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+
+const MAGIC_NUMBER = 3;
 
 class Feedback extends React.Component {
   render() {
+    const { assertions } = this.props;
     return (
-      <Header />
+      <>
+        <Header />
+        <h3 data-testid="feedback-text">
+          { assertions >= MAGIC_NUMBER ? 'Mandou bem!' : 'Podia ser melhor...' }
+        </h3>
+      </>
     );
   }
 }
 
-export default Feedback;
+const mapStateToProps = (state) => ({
+  assertions: state.scoreReducer.assertions,
+});
+
+Feedback.propTypes = {
+  assertions: PropTypes.number.isRequired,
+};
+
+export default connect(mapStateToProps)(Feedback);
