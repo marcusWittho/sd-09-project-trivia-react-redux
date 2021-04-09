4fa0c2c10625121cf5c461bd43395b5e116ca011
alailsonko <aalailson3@gmail.com>
2021-04-09 12:10:40 -0300

    add a timer

--- src/components/CardQuestion.jsx
+++ src/components/CardQuestion.jsx
@@ -1,34 +1,89 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import './CardQuestion.css';
 
 class CardQuestion extends React.Component {
+  constructor(state) {
+    super(state);
+    this.state = {
+      isSelected: false,
+      time: {},
+      seconds: 30,
+    };
+    this.timer = 0;
+    this.startTimer = this.startTimer.bind(this);
+    this.countDown = this.countDown.bind(this);
+    this.selectAnswer = this.selectAnswer.bind(this);
+  }
+
+  componentDidMount() {
+    this.startTimer();
+  }
+
+  selectAnswer() {
+    this.setState({ isSelected: true });
+  }
+
+  secondsToTime(secs) {
+    const obj = {
+      s: secs,
+    };
+    return obj;
+  }
+
+  startTimer() {
+    const { seconds } = this.state;
+    const TIME_INTERVAL = 1000;
+    if (this.timer === 0 && seconds > 0) {
+      this.timer = setInterval(this.countDown, TIME_INTERVAL);
+    }
+  }
+
+  countDown() {
+    const { seconds } = this.state;
+    if (seconds >= 1) {
+      const sec = seconds - 1;
+      this.setState({
+        time: this.secondsToTime(sec),
+        seconds: sec,
+      });
+    }
+    if (seconds === 0) {
+      clearInterval(this.timer);
+    }
+  }
+
   render() {
     const { getQuestions: { questions: { results } } } = this.props;
-
+    const { isSelected, time } = this.state;
     // Constantes criadas para avaliacao do requisito 6. Deleta-las posteriormente.
     const index = 0;
     const currentQuestion = results[index];
-
     // return results.map((currentQuestion, index) => (
     return ( // Return de apenas 1 pergunta para avaliacao do requisito 6. Deletar este return quando houver o botao de proxima pergunta.
       <div key={ index }>
+        <div>
+          {time.s}
+        </div>
         <h2 data-testid="question-category">{currentQuestion.category}</h2>
         <p data-testid="question-text">{currentQuestion.question}</p>
-
         <button
           data-testid="correct-answer"
           type="button"
+          disabled={ time.s === 0 }
+          className={ isSelected || time.s === 0 ? 'correctAnswer' : '' }
           onClick={ this.selectAnswer }
         >
           {currentQuestion.correct_answer}
         </button>
-
         {currentQuestion.incorrect_answers.map((incorrectAnswer, answerIndex) => (
           <button
             data-testid={ `wrong-answer-${answerIndex}` }
             key={ answerIndex }
+            className={ isSelected || time.s === 0 ? 'wrongAnswer' : '' }
             type="button"
+            disabled={ time.s === 0 }
             onClick={ this.selectAnswer }
           >
             {incorrectAnswer}
@@ -39,7 +94,6 @@ class CardQuestion extends React.Component {
     // ));
   }
 }
-
 CardQuestion.propTypes = {
   getQuestions: PropTypes.shape({
     loading: PropTypes.bool,
@@ -49,7 +103,6 @@ CardQuestion.propTypes = {
     }),
   }),
 };
-
 CardQuestion.defaultProps = {
   getQuestions: PropTypes.shape({
     loading: PropTypes.bool,
@@ -59,9 +112,7 @@ CardQuestion.defaultProps = {
     }),
   }),
 };
-
 const mapStateToProps = (state) => ({
   getQuestions: state.questions,
 });
-
 export default connect(mapStateToProps)(CardQuestion);
