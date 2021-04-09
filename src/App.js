9a57f41c295909226a89c431357c92abb086338a
Felipe Shcolnik <shcolnik@gmail.com.br>
2021-04-09 18:08:50 -0300

    task 12

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router';
 import Login from './pages/Login';
 import Settings from './pages/Settings';
 import Question from './pages/Question';
+import Feedback from './pages/Feedback';
 // import logo from './trivia.png';
 import './App.css';
 
@@ -12,6 +13,7 @@ export default function App() {
       <Route exact path="/" component={ Login } />
       <Route path="/settings" component={ Settings } />
       <Route path="/question" component={ Question } />
+      <Route path="/feedback" component={ Feedback } />
     </Switch>
   );
 }
---
+++ src/pages/Feedback.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { string, objectOf } from 'prop-types';
+
+class Feedback extends React.Component {
+  constructor(props) {
+    super(props);
+    this.createHeader = this.createHeader.bind(this);
+  }
+
+  createHeader() {
+    const { playerState: { name, score, gravatarEmail } } = this.props;
+    console.log('aparece');
+    return (
+      <header>
+        <img
+          src={ `https://www.gravatar.com/avatar/${gravatarEmail}` }
+          alt="imagem do Gravatar"
+          data-testid="header-profile-picture"
+        />
+        <h4 data-testid="header-player-name">{ name }</h4>
+        <h4 data-testid="header-score">{ score }</h4>
+        <h2>sss</h2>
+      </header>
+    );
+  }
+
+  render() {
+    return (this.createHeader());
+  }
+}
+
+const mapStateToProps = ((state) => ({
+  playerState: state.player,
+}));
+
+Feedback.propTypes = {
+  playerState: objectOf({
+    name: string,
+    gravatarEmail: string,
+  }),
+}.isRequired;
+
+export default connect(mapStateToProps, null)(Feedback);
