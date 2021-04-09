04b6c712f58a848e03ec307421ce9fd67f4b77ff
Henrique Brito Elias <henriquebelias@gmail.com>
2021-04-09 16:02:44 -0300

    main-group-18 -> requisito 12

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from '../components/Header';
 
 class Feedback extends React.Component {
   render() {
     return (
-      <h1 data-testid="feedback-text">Feedback</h1>
+      <Header />
     );
   }
 }
