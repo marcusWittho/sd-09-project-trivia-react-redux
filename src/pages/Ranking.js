057a5115af69498c70351df4e2ed3261fc322b81
Rafa Reis <rafaelrnascimento2006@gmail.com>
2021-04-10 14:43:27 -0300

    corrigindo issues do lint

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -7,10 +7,7 @@ class Ranking extends React.Component {
   constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
-    this.state = {
-      shouldRedirect: false,
-      ranking: []
-    }
+    this.state = { shouldRedirect: false,  ranking: [] }
   }
 
   handleClick() {
@@ -19,18 +16,18 @@ class Ranking extends React.Component {
 
   render() {
     const { shouldRedirect,  } = this.state;
-    const ranking = JSON.parse(localStorage.getItem('ranking'))
+    const ranking = JSON.parse(localStorage.ranking);
     if (shouldRedirect) return <Redirect to="/" />;
     return (
       <div>
         <ol>
-          {ranking.map((tag, index) => (
-              <li>
-                <img src={ `https://www.gravatar.com/avatar/${tag.token}` } alt="user-image"/>
-                <h2 data-testid={`player-name-${index}`} >{ tag.name }</h2>
-                <h3 data-testid={`player-score-${index}`}>{ tag.score }</h3>
-              </li>
-            ))}
+          {ranking.map((player, index) => (
+            <li key={ index }>
+              <img src={ `https://www.gravatar.com/avatar/${player.token}` } alt={ player.name } />
+              <h2 data-testid={ `player-name-${index}` }>{ player.name }</h2>
+              <h3 data-testid={ `player-score-${index}` }>{ player.score }</h3>
+            </li>
+          ))}
         </ol>
         <button
           data-testid="btn-go-home"
