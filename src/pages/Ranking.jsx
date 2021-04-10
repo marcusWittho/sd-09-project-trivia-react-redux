22dcfe8d81d924ad0fb55bab9b401725b8e30f8a
Micaela Alves <micaela.alves93@gmail.com>
2021-04-10 10:09:13 -0300

    estrutura interna de ranking page

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -2,8 +2,24 @@ import React, { Component } from 'react';
 
 class Ranking extends Component {
   render() {
+    const ranking = JSON.parse(localStorage.getItem('ranking'));
+    const rankingList = ranking
+      .sort((x, y) => y.score - x.score)
+      .map((gamer, index) => (
+        <li key="index">
+          <img src={ gamer.picture } alt="Imagem do Jogador" />
+          <h3 data-testid={ `player-name${index}` }>{ gamer.name }</h3>
+          <p data-testid={ `player-score-${index}` }>{ gamer.score }</p>
+        </li>
+      ));
+
     return (
-      <h1 data-testid="ranking-title">Ranking</h1>
+      <section data-testid="ranking-title">
+        <h1>Ranking</h1>
+        <ul>
+          { rankingList }
+        </ul>
+      </section>
     );
   }
 }
