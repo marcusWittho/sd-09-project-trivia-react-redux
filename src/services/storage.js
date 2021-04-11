83ca829d5537a78d4aff3b642880dcff12ac17cc
Celso Oliva Aparecido <celso.oliva.aparecido@gmail.com>
2021-04-11 16:57:00 -0300

    chore(services/storage): create localStorage services
    Local storage handlers for user info and updates of score given the new one. Relocating this from reducer to component context.

---
+++ src/services/storage.js
@@ -0,0 +1,19 @@
+export const userStorage = ({ name, email }) => {
+  const userLocalStorage = {
+    player: {
+      name,
+      assertions: 0,
+      score: 0,
+      gravatarEmail: email,
+    },
+  };
+  localStorage.setItem('state', JSON.stringify(userLocalStorage));
+};
+
+export const scoreStorage = (newScore) => {
+  const { player } = JSON.parse(localStorage.getItem('state'));
+  const newLocalStorageState = JSON.stringify({
+    player: { ...player, score: newScore },
+  });
+  localStorage.setItem('state', newLocalStorageState);
+};
