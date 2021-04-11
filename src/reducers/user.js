44d1c3d8dac08bd0d8a806dffeacde3e0e2fcf17
Celso Oliva Aparecido <celso.oliva.aparecido@gmail.com>
2021-04-11 15:13:46 -0300

    refactor(reducers/user): cria funções novo score
    funções para calcular o novo estado baseado nas dificuldades e tempo de resposta da questão e returnar o acumulado do score além de salvar o novo score no localStorage. As funções foram retiradas da action para o reducer calcular o novo estado baseado na action.

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,4 +1,26 @@
-import { ADD_USER_INFO, SET_SCORE } from '../actions/types';
+import { ADD_USER_INFO, ANSWER_CORRECT } from '../actions/types';
+
+const CORRECT_CONST = 10;
+const SCORE_BY_CATEGORY = {
+  easy: 1,
+  medium: 2,
+  hard: 3,
+};
+
+const scoreStorage = (stateScore, thisScore) => {
+  const { player } = JSON.parse(localStorage.getItem('state'));
+  const newScore = stateScore + thisScore;
+  const newLocalStorageState = JSON.stringify({
+    player: { ...player, score: newScore },
+  });
+  localStorage.setItem('state', newLocalStorageState);
+};
+
+const calcNewScore = ({ difficulty, THISANSWEREDTIMER }, { score: stateScore }) => {
+  const thisScore = CORRECT_CONST + THISANSWEREDTIMER * SCORE_BY_CATEGORY[difficulty];
+  scoreStorage(stateScore, thisScore);
+  return stateScore + thisScore;
+};
 
 const INITIAL_STATE = {
   name: '',
@@ -13,8 +35,8 @@ const addUserInfoReducer = (state = INITIAL_STATE, action) => {
       ...state,
       ...action.payload,
     };
-  case SET_SCORE:
-    return { ...state, score: action.payload };
+  case ANSWER_CORRECT:
+    return { ...state, score: calcNewScore(action.payload, state) };
   default:
     return state;
   }
