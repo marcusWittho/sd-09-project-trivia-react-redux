8858a0d3ae260eb059cef2b3e09b3d44e7e0180d
Celso Oliva Aparecido <celso.oliva.aparecido@gmail.com>
2021-04-11 15:09:45 -0300

    refactor(actions/types): renomeia constante  ANSWER_CORRECT que lida com score.
    Maior clareza do type da action que altera o score do usuário

--- src/actions/types.js
+++ src/actions/types.js
@@ -2,4 +2,4 @@ export const ADD_USER_INFO = 'ADD_USER_INFO';
 export const REQUEST_TRIVIA = 'REQUEST_TRIVIA';
 export const GET_TRIVIA = 'GET_TRIVIA';
 export const FAILED_TRIVIA_REQUEST = 'FAILED_TRIVIA_REQUEST';
-export const SET_SCORE = 'SET_SCORE';
+export const ANSWER_CORRECT = 'ANSWER_CORRECT';
