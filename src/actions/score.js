3ac1210d94554e1f8c6402b68e242f5054996d28
Celso Oliva Aparecido <celso.oliva.aparecido@gmail.com>
2021-04-11 15:05:05 -0300

    refactor(actions/score): calculate new state by reducer
    Before this we have our payload with the calculated value, somekind of breaking the structure of redux where the reducer is the one with this responsability. Actions should have information about whats is the change we want the reducers will do. These functions will be at reducers/user

--- src/actions/score.js
+++ src/actions/score.js
@@ -1,31 +1,8 @@
-import { SET_SCORE } from './types';
-
-const CORRECT_CONST = 10;
-
-const SCORE_BY_CATEGORY = {
-  easy: 1,
-  medium: 2,
-  hard: 3,
-};
-
-const localScore = (thisScore) => {
-  const { player } = JSON.parse(localStorage.getItem('state'));
-  const newScore = player.score + thisScore;
-  const newState = JSON.stringify({
-    player: { ...player, score: newScore },
-  });
-  localStorage.setItem('state', newState);
-};
-
-const calcThisScore = ({ difficulty, THISANSWEREDTIMER }) => {
-  const thisScore = CORRECT_CONST + THISANSWEREDTIMER * SCORE_BY_CATEGORY[difficulty];
-  localScore(thisScore);
-  return thisScore;
-};
+import { ANSWER_CORRECT } from './types';
 
 const scoreThisCorrectAnswer = (answeredCorrect) => ({
-  type: SET_SCORE,
-  payload: calcThisScore(answeredCorrect),
+  type: ANSWER_CORRECT,
+  payload: answeredCorrect,
 });
 
 export default scoreThisCorrectAnswer;
