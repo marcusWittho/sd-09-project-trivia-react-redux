e14a85d701fa79099e61f7eb6b704309aeee1b00
Celso Oliva Aparecido <celso.oliva.aparecido@gmail.com>
2021-04-11 15:45:33 -0300

    refactor(actions/index): remove serviço de localstorage da action creator
    Removida a função que salvava os dados do usuário em um localStorage, mantendo o uso da action creator apenas para carregar a informação que será usada para o cálculo do novo estado pelo Reducer. Na atualização de estado, teremos o cálculo e a chamada para a persistencia no localStorage será movida para o contexto do componente.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,21 +1,8 @@
 import { ADD_USER_INFO } from './types';
 
-const localUser = (info) => {
-  const newLocalState = {
-    player: {
-      name: info.name,
-      assertions: 0,
-      score: 0,
-      gravatarEmail: info.email,
-    },
-  };
-  localStorage.setItem('state', JSON.stringify(newLocalState));
-  return info;
-};
-
 const addUserInfo = (info) => ({
   type: ADD_USER_INFO,
-  payload: localUser(info),
+  payload: info,
 });
 
 export default addUserInfo;
