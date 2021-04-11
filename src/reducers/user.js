df095a1a23b9b3f1f9e81559f8e6698a4f27cb75
Celso Oliva Aparecido <celso.oliva.aparecido@gmail.com>
2021-04-11 16:50:19 -0300

    refactor(reducers/user.js): connect with score service
    Connect reducer user with the function score at services where the newScore is returned for the reducer. Some localStorage also is moved from here to its own files.

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,26 +1,5 @@
 import { ADD_USER_INFO, ANSWER_CORRECT } from '../actions/types';
-
-const CORRECT_CONST = 10;
-const SCORE_BY_CATEGORY = {
-  easy: 1,
-  medium: 2,
-  hard: 3,
-};
-
-const scoreStorage = (stateScore, thisScore) => {
-  const { player } = JSON.parse(localStorage.getItem('state'));
-  const newScore = stateScore + thisScore;
-  const newLocalStorageState = JSON.stringify({
-    player: { ...player, score: newScore },
-  });
-  localStorage.setItem('state', newLocalStorageState);
-};
-
-const calcNewScore = ({ difficulty, THISANSWEREDTIMER }, { score: stateScore }) => {
-  const thisScore = CORRECT_CONST + THISANSWEREDTIMER * SCORE_BY_CATEGORY[difficulty];
-  scoreStorage(stateScore, thisScore);
-  return stateScore + thisScore;
-};
+import calcNewScore from '../services/score';
 
 const INITIAL_STATE = {
   name: '',
@@ -28,7 +7,7 @@ const INITIAL_STATE = {
   score: 0,
 };
 
-const addUserInfoReducer = (state = INITIAL_STATE, action) => {
+const userInfoReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
   case ADD_USER_INFO:
     return {
@@ -36,10 +15,13 @@ const addUserInfoReducer = (state = INITIAL_STATE, action) => {
       ...action.payload,
     };
   case ANSWER_CORRECT:
-    return { ...state, score: calcNewScore(action.payload, state) };
+    return {
+      ...state,
+      ...calcNewScore(action.payload, state),
+    };
   default:
     return state;
   }
 };
 
-export default addUserInfoReducer;
+export default userInfoReducer;
