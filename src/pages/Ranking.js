4dddbed56a2c9c7b76e7359c7e29411132a0a6ab
Pedro Amaral <amaralph1995@gmail.com>
2021-04-12 19:33:57 -0300

    [FEAT][Cria pagina Ranking][Req-17][V1.0]

---
+++ src/pages/Ranking.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import './ranking.css';
+
+class Ranking extends Component {
+  render () {
+
+    // recupera o objeto JSON do localStorage
+    const jsonState = localStorage.getItem('state');
+
+    // converte o objeto JSON pra um array
+    const ranking = JSON.parse(jsonState).ranking;
+
+    return (
+      <div className="card-container">
+        <h2>Ranking</h2>   
+        <ul className="ranking-list">
+          {
+            ranking.sort((a, b) => a.score < b.score ? 1 : -1)
+            .map((jogada, index) =>
+              <li key={ jogada.name } className="ranking-item">
+                <div>
+                  <div data-testid={ `player-name-${index}` }>
+                    <span className="ranking-text">Nome:</span> { jogada.name }
+                  </div>
+                  <div data-testid={ `player-score-${index}` }>
+                    <span className="ranking-text">Score:</span> { jogada.score }
+                  </div>
+                </div>
+                <img src={ jogada.picture } alt="foto do jogador" />
+              </li>
+            )
+          }
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default Ranking;
