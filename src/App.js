ff67f22de69ec0fbd0b9f92a1d50895ba9be3c4d
Felipe Cardoso <felipedclc@gmail.com>
2021-04-12 14:22:13 -0300

    reqs 12,13,14

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import logo from './trivia.png';
 import Login from './pages/Login';
 import Game from './pages/Game';
 import Settings from './pages/Settings';
+import Feedback from './pages/Feedback';
 
 import './App.css';
 
@@ -16,6 +17,7 @@ export default function App() {
           <Route exact path="/" component={ Login } />
           <Route exact path="/game" component={ Game } />
           <Route exact path="/settings" component={ Settings } />
+          <Route exact path="/feedback" component={ Feedback } />
         </Switch>
       </header>
     </div>
