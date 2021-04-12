f094c92da2be6d17475c25b7e1c823af1d4801e5
Pedro de Almeida <ped.alm2709@gmail.com>
2021-04-12 17:56:33 -0300

    requisito 14

--- src/components/MainFeedBack.js
+++ src/components/MainFeedBack.js
@@ -1,22 +1,17 @@
 import React from 'react';
 
 class MainFeedBack extends React.Component {
-  getAssertions() {
-    const assertions = localStorage.getItem('state');
-    JSON.parse(assertions);
-    console.log(JSON.parse(assertions));
-  }
 
   render() {
     const assertions = JSON.parse(localStorage.getItem('state'));
     const minAssertions = 3;
     return (
-      <h1>
-        { this.getAssertions() }
+      <>
+        <h2 data-testid="feedback-total-question">{ assertions.player.assertions }</h2>
         {assertions.player.assertions < minAssertions
           ? <p data-testid="feedback-text">Podia ser melhor...</p>
           : <p data-testid="feedback-text">Mandou bem!</p>}
-      </h1>
+      </>
     );
   }
 }
