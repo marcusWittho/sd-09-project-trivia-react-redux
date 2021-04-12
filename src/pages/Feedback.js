488c222bc45912a414685c0c2fa4b5a1f4872ad2
Caio <caioqs.info@gmail.com>
2021-04-12 17:49:45 -0300

    Feito requisito 13

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -4,11 +4,27 @@ import { connect } from 'react-redux';
 import Header from './components/Header';
 
 class Feedback extends Component {
+  constructor(props) {
+    super(props);
+    this.menssageFeedBack = this.menssageFeedBack.bind(this);
+  }
+
+  menssageFeedBack() {
+    const numberThree = 3;
+    const { assertions } = this.props;
+    console.log(typeof (assertions));
+    if (assertions < numberThree) {
+      console.log(assertions);
+      return (<p data-testid="feedback-text">Podia ser melhor...</p>);
+    }
+    return (<p data-testid="feedback-text">Mandou bem!</p>);
+  }
+
   render() {
     return (
       <div>
         <Header />
-        <p data-testid="feedback-text">Feedback</p>
+        {this.menssageFeedBack()}
       </div>
     );
   }
@@ -17,11 +33,13 @@ class Feedback extends Component {
 const mapStateToProps = (state) => ({
   name: state.loginUser.name,
   email: state.loginUser.email,
+  assertions: state.player.assertions,
 });
 
 Feedback.propTypes = {
   email: PropTypes.string,
   name: PropTypes.string,
+  assertions: PropTypes.number,
 }.isRequired;
 
 export default connect(mapStateToProps)(Feedback);
