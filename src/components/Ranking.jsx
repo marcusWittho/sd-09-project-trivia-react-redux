cf78778e21ce5be735dc2f9518eccdc35b33f0eb
Daniel Fasanaro de Carvalho <danielfasanaro@gmail.com>
2021-04-12 13:47:04 -0300

    Update src/components/Ranking.jsx

--- src/components/Ranking.jsx
+++ src/components/Ranking.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class Ranking extends React.Component {
   render() {
     return (
-      <p data-testid="ranking-title">Randking</p>
+      <p data-testid="ranking-title">Ranking</p>
     );
   }
 }
