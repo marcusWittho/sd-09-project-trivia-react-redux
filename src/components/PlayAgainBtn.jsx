7af183c40669e6f7414290229dd89b30fb296976
Enio Nicael <enionicael@gmail.com>
2021-04-12 11:28:48 -0300

    Cria o componente PlayAgainBtn.

---
+++ src/components/PlayAgainBtn.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class PlayAgainBtn extends React.Component {
+  render() {
+    return (
+      <button
+        type="button"
+        onClick={ this.handleClickBtn }
+        data-testid="btn-play-again"
+        >Jogar novamente</button>
+    );
+  }
+}
+
+export default PlayAgainBtn;
--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../components/Header';
+import PlayAgainBtn from '../components/PlayAgainBtn';
 
 class Feedback extends React.Component {
   render() {
     return (
       <div>
         <Header />
+        <PlayAgainBtn />
       </div>
     );
   }
