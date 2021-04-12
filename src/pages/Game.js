1f9cf38e6708522a2ffea86f2bd3f3712bec1fab
Lucas Silva <martssilvapro@gmail.com>
2021-04-12 17:51:08 -0300

    [ESTILIZACAO][ajustes na seleçao de elemento][V4]

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import './game.css';
