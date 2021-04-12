3404f3d29df9a1c16defd086d03ed84a7186a5dc
Juliana do Carmo <julianadocarmoti@gmail.com>
2021-04-12 19:55:05 -0300

    requisito 13 -feedback

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import { questionsRequest } from '../services/api';
 import { GET_PLAYER_NAME, GET_PLAYER_EMAIL, GET_TOKEN,
-  GET_QUESTIONS, UPDATE_INDEX, UPDATE_SCORE_ASSERTIONS } from './actionTypes';
+  GET_QUESTIONS, UPDATE_INDEX, UPDATE_SCORE_ASSERTIONS,
+  RESET_PLAYER, RESET_REQUEST } from './actionTypes';
 
 export const updateToken = (token) => ({
   type: GET_TOKEN,
@@ -39,3 +40,11 @@ export function fetchQuestions(token) {
       .then((data) => dispatch(updateQuestions(data)))
   );
 }
+
+export const resetPlayer = () => ({
+  type: RESET_PLAYER,
+});
+
+export const resetRequest = () => ({
+  type: RESET_REQUEST,
+});
--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,21 +1,68 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import Header from '../Componentes/Header';
+import { resetPlayer, resetRequest } from '../actions';
 
 class Feedback extends React.Component {
+  constructor() {
+    super();
+
+    this.resetScore = this.resetScore.bind(this);
+  }
+
+  resetScore() {
+    const { resetPlayerStore, resetRequestStore } = this.props;
+    resetPlayerStore();
+    resetRequestStore();
+  }
+
   render() {
+    const { assertions, score } = this.props;
+    const three = 3;
     return (
       <div>
         <Header />
+        <p data-testid="feedback-text">
+          {assertions >= three
+            ? 'Mandou bem!'
+            : 'Podia ser melhor...'}
+        </p>
+        <h1>Pontuação: </h1>
+        <p data-testid="feedback-total-score">{score}</p>
+        <h4> Perguntas acertadas: </h4>
+        <p data-testid="feedback-total-question">{assertions}</p>
         <Link to="/ranking">
           <button type="button" data-testid="btn-ranking">Ranking</button>
         </Link>
         <Link to="/">
-          <button type="button" data-testid="btn-play-again">Jogar Novamente</button>
+          <button type="button" data-testid="btn-play-again" onClick={ this.resetScore }>
+            Jogar Novamente
+          </button>
         </Link>
+
       </div>
     );
   }
 }
 
-export default Feedback;
+Feedback.propTypes = {
+  assertions: PropTypes.number.isRequired,
+  resetPlayerStore: PropTypes.func.isRequired,
+  resetRequestStore: PropTypes.func.isRequired,
+  score: PropTypes.number.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  assertions: state.player.assertions,
+  score: state.player.score,
+  ranking: state.ranking,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  resetPlayerStore: () => dispatch(resetPlayer()),
+  resetRequestStore: () => dispatch(resetRequest()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Feedback);
