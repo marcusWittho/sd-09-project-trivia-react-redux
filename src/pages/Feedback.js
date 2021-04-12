65b619f66589d6ce949a7708009a24c65335e44e
Caio <caioqs.info@gmail.com>
2021-04-12 17:23:23 -0300

    Feito requisito 12

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Header from './components/Header';
 
 class Feedback extends Component {
   render() {
     return (
       <div>
-        <h1>Feedback</h1>
+        <Header />
         <p data-testid="feedback-text">Feedback</p>
       </div>
     );
   }
 }
 
-export default Feedback;
+const mapStateToProps = (state) => ({
+  name: state.loginUser.name,
+  email: state.loginUser.email,
+});
+
+Feedback.propTypes = {
+  email: PropTypes.string,
+  name: PropTypes.string,
+}.isRequired;
+
+export default connect(mapStateToProps)(Feedback);
