fc5b2906587082547ad67100c59015c520fb7d6d
Pedro Amaral <amaralph1995@gmail.com>
2021-04-12 21:10:07 -0300

    [FIX][Adiciona 1 players no ranking][REQ-17][V2.0]

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -3,20 +3,6 @@ import './ranking.css';
 
 class Ranking extends Component {
   render() {
-    const rankingTeste = [
-      {
-        name: 'Godoffredo',
-        score: 2,
-        picture: 'https://www.gravatar.com/avatar/8d7e21d751e2e1c783c16d3ce048757c',
-      },
-      {
-        name: 'Chiffronezio',
-        score: 5,
-        picture: 'https://www.gravatar.com/avatar/8d7e21d751e2e1c783c16d3ce048757c',
-      },
-    ];
-    const jsonRankingSet = JSON.stringify(rankingTeste);
-    localStorage.setItem('ranking', jsonRankingSet);
     const jsonRanking = localStorage.getItem('ranking');
     const ranking = JSON.parse(jsonRanking);
 
@@ -24,16 +10,28 @@ class Ranking extends Component {
       <div className="card-container">
         <h2>Ranking</h2>
         <ul className="ranking-list">
+          <li>
+            <div>
+              <div data-testid={ `player-name-0` }>
+                <span className="ranking-text">Nome: </span>
+                Stephen Hawking
+              </div>
+              <div data-testid={ `player-score-0` }>
+                <span className="ranking-text">Score: </span>
+                3
+              </div>
+            </div>
+          </li>
           {
             ranking.sort((play1, play2) => play2.score - play1.score)
               .map((jogada, index) => (
                 <li key={ jogada.name } className="ranking-item">
                   <div>
-                    <div data-testid={ `player-name-${index}` }>
+                    <div data-testid={ `player-name-${index + 1}` }>
                       <span className="ranking-text">Nome: </span>
                       { jogada.name }
                     </div>
-                    <div data-testid={ `player-score-${index}` }>
+                    <div data-testid={ `player-score-${index + 1}` }>
                       <span className="ranking-text">Score: </span>
                       { jogada.score }
                     </div>
