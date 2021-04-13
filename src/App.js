ead4bc81394a8cd84b0f4dc6b5e9c34c080b6839
Thiago Granville <thiagogranville@gmail.com>
2021-04-13 00:10:17 -0300

    [Feat][Requisito 11][Adiciona redirecionamento para Feedback][V1.0]

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Game from './pages/Game';
 import Settings from './pages/Settings';
+import Feedback from './pages/Feedback';
 import './App.css';
 
 export default function App() {
@@ -11,6 +12,7 @@ export default function App() {
       <Route exact path="/" component={ Login } />
       <Route path="/game" component={ Game } />
       <Route path="/settings" component={ Settings } />
+      <Route path="/feedback" component={ Feedback } />
     </Switch>
   );
 }
