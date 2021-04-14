bd4cf567939a7eeb649b0f28121cf3f3fa1a9bf3
Raquel Picanço <picanco.raquel@gmail.com>
2021-04-14 08:57:00 -0400

    Cria Go Home button

--- src/pages/RankingScreen.jsx
+++ src/pages/RankingScreen.jsx
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
+import { shape, func } from 'prop-types';
 import Header from '../components/Header';
 import RankingList from '../components/Ranking';
 
 export default class RankingScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    const { history } = this.props;
+    history.push('/');
+  }
+
   render() {
     return (
       <>
         <Header />
-        <button type="button" data-testid="btn-go-home">Voltar para home</button>
+        <button
+          type="button"
+          data-testid="btn-go-home"
+          onClick={ this.handleClick }
+        >
+          Voltar para home
+        </button>
         <RankingList />
       </>
     );
   }
 }
+
+RankingScreen.propTypes = {
+  history: shape({
+    push: func,
+  }),
+}.isRequired;
