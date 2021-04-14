53505aa671f5ef82578077b70625f824e691aef3
vinicius <viniciuspalenske@gmail.com>
2021-04-14 20:57:16 -0300

    iniciando requisito 11

---
+++ src/pages/EndGame.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class EndGame extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Podia ser melhor</p>
+        <p>Você acertou 2 questões</p>
+        <p>Um total de 20 pontos</p>
+        <h2>VER RANKING</h2>
+        <button type="button">JOGAR NOVAMENTE</button>
+      </div>
+    );
+  }
+}
+
+export default EndGame;
