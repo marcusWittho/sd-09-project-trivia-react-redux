453c9e21cd939b9341c875c7cf9fda98448c1c16
vinicius <viniciuspalenske@gmail.com>
2021-04-15 19:30:40 -0300

    [resolvendo strings]

--- src/services/triviaApi.js
+++ src/services/triviaApi.js
@@ -1,3 +1,12 @@
+function replaceCode(string) {
+  return string
+    .replace(/&amp;/g, '&')
+    .replace(/&lt;/g, '<')
+    .replace(/&gt;/g, '>')
+    .replace(/&quot;/g, '"')
+    .replace(/&#039;/g, '\'');
+}
+
 export const getToken = async () => {
   const endPoint = 'https://opentdb.com/api_token.php?command=request';
   const response = await fetch(endPoint);
@@ -10,6 +19,7 @@ export const getAnswer = async (number, token) => {
   const response = await fetch(endPoint);
   const result = await response.json();
   result.results.forEach((question) => {
+    question.question = replaceCode(question.question);
     const { correct_answer: correct, incorrect_answers: incorrect } = question;
     const RANDOM = 0.5;
     const sortedOptions = [...incorrect, correct].sort(() => RANDOM - Math.random());
@@ -17,33 +27,3 @@ export const getAnswer = async (number, token) => {
   });
   return result;
 };
-
-export const apiMock = (() => {
-  const response = {
-    response_code: '0',
-    results: [
-      {
-        category: 'Entertainment: Video Games',
-        type: 'multiple',
-        difficulty: 'easy',
-        question: 'What is the first weapon you acquire in Half-Life?',
-        correct_answer: 'A crowbar',
-        incorrect_answers: [
-          'A pistol',
-          'The H.E.V suit',
-          'Your fists',
-        ],
-      },
-      {
-        category: 'Entertainment:  Video Games',
-        type: 'boolean',
-        difficulty: 'hard',
-        question: `{TF2: Sentry rocket damage falloff is calculated based on the} +
-        {distance between the sentry and the enemy, not the engineer and the enemy')}`,
-        correct_answer: 'False',
-        incorrect_answers: ['True'],
-      },
-    ],
-  };
-  return response;
-});
