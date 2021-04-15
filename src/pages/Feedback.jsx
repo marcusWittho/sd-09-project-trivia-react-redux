8cac1e642c35fc1074e78ecec2d355ae0b055b37
Felipe Shcolnik <shcolnik@gmail.com.br>
2021-04-15 13:20:02 -0300

    task 14 ok

---
+++ src/pages/Feedback.jsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { string, objectOf } from 'prop-types';
+
+class Feedback extends React.Component {
+  constructor(props) {
+    super(props);
+    this.createHeader = this.createHeader.bind(this);
+    this.FeedbackMessage = this.FeedbackMessage.bind(this);
+    this.gameResume = this.gameResume.bind(this);
+  }
+
+  createHeader() {
+    const state = JSON.parse(localStorage.getItem('state'));
+    if (state) {
+      const { name, score, gravatarEmail } = state.player;
+      return (
+        <header>
+          <img
+            src={ `https://www.gravatar.com/avatar/${gravatarEmail}` }
+            alt="imagem do Gravatar"
+            data-testid="header-profile-picture"
+          />
+          <h4 data-testid="header-player-name">{ name }</h4>
+          <h4 data-testid="header-score">{ score }</h4>
+        </header>
+      );
+    }
+  }
+
+  FeedbackMessage() {
+    const { assertions } = this.props;
+    const playerScore = 3;
+    if (assertions < playerScore) {
+      return <p data-testid="feedback-text">Podia ser melhor...</p>;
+    } return <p data-testid="feedback-text">Mandou bem!</p>;
+  }
+
+  gameResume() {
+    const { assertions } = this.props;
+    const state = JSON.parse(localStorage.getItem('state'));
+    // if (state) {
+    //   const { name, score, gravatarEmail } = state.player;
+    // if (assertions === 0) {
+    //   return <h5 data-testid="feedback-total-question">Não acertou nenhuma pergunta</h5>
+    return (
+      <div className="total-score">
+        <h5>
+          Seu placar foi de
+          <span data-testid="feedback-total-score">{state.player.score}</span>
+          pontos
+        </h5>
+        <h5>
+          Acertou
+          <span data-testid="feedback-total-question">{assertions}</span>
+          perguntas
+        </h5>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <>
+        {/* { this.createHeader() } */}
+        <p data-testid="feedback-text">FEEDBACK</p>
+        { this.FeedbackMessage() }
+        { this.gameResume() }
+      </>
+    );
+  }
+}
+
+const mapStateToProps = ((state) => ({
+  assertions: state.player.assertions,
+}));
+
+Feedback.propTypes = {
+  playerState: objectOf({
+    name: string,
+    gravatarEmail: string,
+  }),
+}.isRequired;
+
+export default connect(mapStateToProps)(Feedback);
